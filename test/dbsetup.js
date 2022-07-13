503d6b7f9e4da6b9a8a67b63986d99c5312d36a2
collins mbathi <perezcoolins@gmail.com>
2022-07-13 13:35:03 +0300

    fixed bug

--- test/dbsetup.js
+++ test/dbsetup.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 
 
 
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, './.env') });
+// const path = require('path');
+// require('dotenv').config({ path: path.resolve(__dirname, './.env') });
  /**
   * connect to the in-memory database.
   */
 
  module.exports.connect = async () => {
-  await mongoose.connect(process.env.MONGO_URL)
+  await mongoose.connect("mongodb+srv://collinsm:0791686851@cluster0.xszl2nd.mongodb.net/?retryWrites=true&w=majority")
    .then(() => console.log("DB connected!"))
    .catch(err => console.log(err));
  }
